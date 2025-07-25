4da0cd759680a8cff5dc0ddbd779be5b58b5e93f
JiHyoung You <fbwlgud51@gmail.com>
2025-07-25 14:15:08 +0900

    proxy setting

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  // Mixed Content 에러
+  // 배포 사이트 주소는 https인데 http로 api를 요청해서 차단됨.
+  // 프록시 만들어서 연결
+  // baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
   headers: {
     "Content-Type": "application/json",
   },
